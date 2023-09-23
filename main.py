356346d194ae0b58bd8e4412203e97b486f2e611
Baptiste <baptistefalbierski7@gmail.com>
2023-09-23 18:59:14 +0200

    adaptation pour des temps avec des secondes

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import time
 import beepy
 
 
-def effectuer_cuisson(index_choix):
-    duree = CUISSONS[index_choix-1][1] * 60
+def effectuer_cuisson(choix_cuisson):
+    duree = choix_cuisson
     print("Cuisson en cours: ", end="", flush=True)
     print()
-    while duree > 0:
-        print("Temps restant: ", end="", flush=True)
-        min = duree // 60
-        sec = duree - min * 60
-        print(f" {min:02d}:{sec:02d}", end="", flush=True)
-        for i in range(10):
+    while True:
+        for i in range(DUREE_PROGRESSION):
             time.sleep(1)
             print(".", end="", flush=True)
+            duree -= 1
+            if duree <= 0:
+                break
+        if duree <= 0:
+            break
+        min = duree // 60
+        sec = duree - min * 60
         print()
-        duree -= 10
+        print(f"Temps restant: {min:02d}:{sec:02d}", end="", flush=True)
+
 
 
 def demander_choix_cuisson(min, max):
@@ -31,26 +35,54 @@ def demander_choix_cuisson(min, max):
         return demander_choix_cuisson(min, max)
 
 
+def obtenir_temps(duree):
+    min = duree // 60
+    sec = duree - min * 60
+    temps = ""
+    min_unit = "minutes"
+    sec_unit = "secondes"
+    if min == 1:
+        min_unit = "minute"
+    if sec == 1:
+        sec_unit = "secconde"
+    if min > 0:
+        temps += f"{min} {min_unit}"
+    if sec > 0:
+        if len(temps) > 0:
+            temps += " "
+        temps += f"{sec} {sec_unit}"
+    return temps
+
+
+
+
+
+
 # --------------------- Programme principale
 
 
+
 CUISSONS = (
-    ("Oeufs à la coque", 3),
-    ("Oeufs mollets", 6),
-    ("Oeufs dur", 9)
+    ("Oeufs à la coque", 3*60),
+    ("Oeufs mollets", 6*60),
+    ("Oeufs dur", 9*60)
 )
+
+# nombre depoint à affichier
+DUREE_PROGRESSION = 10
 title = "Choix de cuisson"
 
 
 print("-"*len(title), title, "-"*len(title))
 index = 1
 for cuisson in CUISSONS:
-    print(f"{index}) {cuisson[0]} ({cuisson[1]} min)")
+    print(f"{index}) {cuisson[0]} ({obtenir_temps(cuisson[1])})")
     index += 1
 
-valeur_max_choix = index - 1
-choix = demander_choix_cuisson(1, valeur_max_choix)
-effectuer_cuisson(choix)
+choix = demander_choix_cuisson(1, len(CUISSONS))
+choix_cuisson = CUISSONS[choix-1][1]
+effectuer_cuisson(choix_cuisson)
+print()
 print("Cuisson términée!")
 beepy.beep(sound="ping")
 
